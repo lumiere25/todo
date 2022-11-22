220bf0449b1c84e7440da1eeb4ef32869000e7fa
lumiere25 <57967828+lumiere25@users.noreply.github.com>
2022-11-22 17:44:34 +1100

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const [ task, setTask ] = useState({
+    id: 0,
+    taskDescription: "",
+    isCompleted: false
+  });
+  const [ todos, setTodos ] = useState([]);
+ 
+ function addTodos(e) {
+   e.preventDefault();
+   setTodos([
+     ...todos,
+     {
+       id: todos.length + 1,
+       taskDescription: task.taskDescription,
+       isCompleted: false
+     }
+   ]);
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h1>Todo List</h1>
+    <form onSubmit={addTodos}>
+     <div>
+     <label>
+      task 
+      <input type="text"
+      value={task.taskDescription}
+      name="taskDescription"
+      onChange={(event) => 
+        setTask({
+          id: 0,
+          taskDescription: "",
+          isCompleted: false
+        });
+      }
+    />
+  </label>
+  <button>Add Todo</button>   
+    </div>  
+ </form>
+ </div>
+
   );
 }
 
