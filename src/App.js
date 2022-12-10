d1e28a266bae6122604082f1f09c7f1bbff77bac
lumiere25 <57967828+lumiere25@users.noreply.github.com>
2022-12-10 11:41:38 +1100

    updated now

--- src/App.js
+++ src/App.js
@@ -1,86 +1,11 @@
-import { useState } from "react";
-import './App.css';
+import React from 'react';
+import "./styles.css";
+import Todo from "./components/todo/todos";
 
-function App() {
-  const [ task, setTask ] = useState({
-    id: 0,
-    taskDescription: "",
-    isCompleted: false
-  });
-  const [ todos, setTodos ] = useState([]);
- 
- function addTodos(e) {
-   e.preventDefault();
-   setTodos([
-     ...todos,
-     {
-       id: todos.length + 1,
-       taskDescription: task.taskDescription,
-       isCompleted: false
-     }
-   ]);
- }
-
+export default function App() {
   return (
-    <div className="App">
-   
- <>
-{todos.length > 0 ? (
-<>...</>
-
- {todos.map((t, index) => {
-   return (
-     <div id={t.id} key={index + t.id} value={t.id}>
-
-     {t.isCompleted ? (
-       <strike>
-        <p>{t.taskDescription}</p>
-       </strike>
-     ) : (
-       <>
-        <p>{t.taskDescription}</p>
-        <button 
-        id={t.id}
-        key={index + t.id}
-        value={t.id}
-        onClick={ () => 
-        console.log("clicked completed")}
-        >
-         complete
-        </button>
-        </>
-        )}
-        </div>
-        );
-        })}
-      </>
-   
-    
-  
-   <h1>Todo List</h1>
-    <form onSubmit={addTodos}>
-     <div>
-     <label>
-      task 
-      <input type="text"
-      value={task.taskDescription}
-      name="taskDescription"
-      onChange={(event) => 
-        setTask({
-          id: 0,
-          taskDescription: "",
-          isCompleted: false
-        })
-      }
-    />
-  </label>
-  <button>Add Todo</button>   
-    </div>  
- </form>
- </div>
-
-
+    <main>
+     <Todo />
+    </main>
   );
 }
-
-export default App;
